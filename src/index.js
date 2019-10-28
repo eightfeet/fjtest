83b64230c4a2822ea241461d15c241f1117531aa
eightfeet <bajiao000000@163.com>
2019-10-28 17:01:51 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const test = function() {
+    return('this is a test!');
+};
+
+class ClassTest {
+    constructor(){
+        this.name = 'ClassTest'
+    }
+
+    test = () => {
+        console.log('this is test!!!');
+    }
+}
+
+console.log(test());
+
+module.exports = ClassTest;
+
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+const DefinePlugin = webpack.DefinePlugin;
+
+let cdn = process.env.PUBLIC_PATH || './';
+
+const evn = process.env.UAT_ENV;
+const isUat = evn === 'uat' ? true : false;
+
+module.exports = (env, argv) => ({
+	entry: './src/index.js',
+	context: path.resolve(__dirname),
+	output: {
+		library: '___Modal___',
+		libraryTarget: 'umd',
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'modal.js',
+		publicPath: argv.mode === 'development' ? '/' : cdn
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                        plugins: ["@babel/plugin-proposal-class-properties"]
+                    }
+				},
+                exclude: '/node_modules/',
+			}
+		]
+	},
+	resolve: {
+		extensions: [".jsx", ".js", ".json", ".less", ".scss", ".css", ".html"],
+		alias: {
+			modules: path.resolve(__dirname, "src/modules"), // used for tests
+			style: path.resolve(__dirname, "src/style"),
+			core: path.resolve(__dirname, "src/core"),
+			"~": path.resolve(__dirname, "src") // root
+		}
+	},
+	devtool: 'source-map',
+	plugins: [
+		new HtmlWebpackPlugin()
+	],
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin({ sourceMap: true }),
+			new OptimizeCSSAssetsPlugin({})
+		]
+	},
+	devServer: {
+		contentBase: path.join(__dirname, 'src'),
+		compress: true,
+		port: 9000,
+		// host: '0.0.0.0',
+		host: 'localhost',
+		publicPath: '/',
+		historyApiFallback: true,
+		proxy: {
+			"/mf": {
+				target: "http://wx-test1.by-health.com",
+				changeOrigin: true
+			}
+		  }
+	}
+});
