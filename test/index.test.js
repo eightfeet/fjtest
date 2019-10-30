a5b1f1a07aa9b4f97cb1c849773561d86a53827b
eightfeet <bajiao000000@163.com>
2019-10-30 10:16:23 +0800

    update

--- test/index.test.js
+++ test/index.test.js
@@ -1,11 +1,11 @@
 // const myTest =  require('./index');
-import myTest, { ClassTest } from './index';
+import myTest, { ClassTest } from '../src/index';
 
-test('myTest', () => {
+it('myTest', () => {
 	expect(myTest()).toBe('this is a test!');
 	expect(myTest(1)).toBe(1);
 });
 
-test('ClassTest', () => {
+it('ClassTest', () => {
 	expect(new ClassTest().test()).toBe('this is test!!!');
 });
